2c40c4c7b46a6a0705702b49b5cb1f670fc74f25
john sebastian marmolejo castro <148784346+Lechattarre@users.noreply.github.com>
2024-10-17 17:12:44 +0200

    Entrega del labIndiv

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,11 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(n1, n2) {
+
+    if (n1 > n2) {
+        return n1
+    }
+    else return n2
+}
 
 
 
@@ -7,15 +13,45 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(array) {
 
+    if (array.length === 0) {
+        return null;
+    }
+
+    let longword = "";
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i].length > longword.length) {
+            longword = array[i];
+        }
+    }
+
+    return longword;
+}
+
+const result = findLongestWord(words);
+console.log(result);
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(array) {
+
+    let multiarray = 0
+
+
+    for (let i = 0; i < array.length; i++) {
+
+        multiarray += array[i]
+
+    }
+
+    return multiarray
+}
 
 
 
@@ -23,7 +59,27 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+
+function averageNumbers(array) {
+
+    if (array == 0) {
+
+        return 0
+
+    }
+
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+
+
+    let average = sum / array.length;
+
+
+    return Math.floor(average);
+}
 
 
 
@@ -31,4 +87,18 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(array, Find) {
+
+    if (array == 0) {
+        return null
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === Find) {
+            return true;
+        }
+
+    }
+    return false
+}
+
